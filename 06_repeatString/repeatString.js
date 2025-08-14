14e7ab3aa9ff15bf137d70a35e68d7ff5dd133fe
1Ali1077 <aa218noer@gmail.com>
2025-08-15 00:03:46 +0300

    Solve: Write a function that simply repeats the string a given number of times:

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
 
+function repeatString(word,times) {
+    let str = "";
+    for (let i = 0; i < times; i++) {
+        str += word;
+    }
+    return str;
 };
+console.log(repeatString("hey",3))
 
 // Do not edit below this line
 module.exports = repeatString;
